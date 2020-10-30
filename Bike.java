0c1007f5fde047cf5304e5b0a6e254dfc8e5bccb
Landon Stobaugh <landon.stobaugh@gmail.com>
2020-10-29 18:05:03 -0700

    Init
    Added Handle bars along with the main Bike

---
+++ Bike.java
@@ -0,0 +1,8 @@
+
+
+class Bike {
+    public static void main(String[] args) {
+		Handle bike_handle = new Handle("Aero bars");
+        System.out.println(bike_handle.handle_type); 
+    }
+}
---
+++ Handle.java
@@ -0,0 +1,11 @@
+class Handle {
+	//Init different variables for the object
+    public String handle_type;
+    
+    public Handle(String type)
+    {
+		//Set values to initialized variables
+		handle_type = type;
+	}
+	
+}

59a279d47a8c2845d6e5502dc8dde250cd2b367a
Landon Stobaugh <landon.stobaugh@gmail.com>
2020-10-29 18:06:57 -0700

    Added wheel file
    Added a Wheel,java anyone can edit it

---
+++ Wheel.java
@@ -0,0 +1,11 @@
+class Wheel {
+	//Init different variables for the object
+    public String wheel_type;
+    
+    public Wheel(String type)
+    {
+		//Set values to initialized variables
+		wheel_type = type;
+	}
+	
+}

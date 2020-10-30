019fccd6cf0e1643aaa857a5b9fd9f12f45b6cf1
Landon Stobaugh <landon.stobaugh@gmail.com>
2020-10-29 18:15:33 -0700

    Edited Bike File
    Just threw the classes and created objects in here.

--- Bike.java
+++ Bike.java
@@ -2,7 +2,14 @@
 
 class Bike {
     public static void main(String[] args) {
+		//The bike's handle
 		Handle bike_handle = new Handle("Aero bars");
-        System.out.println(bike_handle.handle_type); 
+		
+		//The bike's brake
+		Brake bike_brake = new Brake("V-Brales");
+		
+		//The bike's wheel type
+		Wheel bike_wheel = new Wheel("Clincher")
+        
     }
 }

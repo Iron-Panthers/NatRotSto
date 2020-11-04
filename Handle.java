cb3b21ae5584be9a70e3efd8fb0b1932ab551cf4
Landon Stobaugh <landon.stobaugh@gmail.com>
2020-11-03 17:43:55 -0800

    Handle Changes
    Added a few features around handles

--- Handle.java
+++ Handle.java
@@ -1,11 +1,23 @@
 class Handle {
-	//Init different variables for the object
+	/*Init different variables for the object*/
+	
+	//Type of handle
     public String handle_type;
     
-    public Handle(String type)
+    //Material of the handle
+    public String handle_material;
+    
+    //Whether the handle has a grip
+    public boolean has_grip;
+    
+    public Handle(String type, String mat, boolean grip)
     {
 		//Set values to initialized variables
 		handle_type = type;
+
+		handle_material = mat;
+
+		has_grip = grip;
 	}
 	
 }

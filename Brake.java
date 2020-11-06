2b940b4088e213cc3cf0a72ffcf20a831f372a41
Landon Stobaugh <landon.stobaugh@gmail.com>
2020-11-05 18:13:54 -0800

    Changes to Brake class
    The brake actually can now brake

--- Brake.java
+++ Brake.java
@@ -6,6 +6,20 @@ class Brake
     //How strong of a break
     public int brake_stren;
     
+    
+    //Functions that tries to stop the bike
+    public boolean attempt_brake()
+    {
+		if (brake_stren >= 10)
+		{
+			System.out.println("Brakes are working! Uses left: "+(brake_stren-10));
+			brake_stren--;
+			return true;
+		}
+		System.out.println("Oh no! Brakes are not working");
+		return false;
+	}
+    
     public Brake(String type, int stren)
     {
 		//Set values to initialized variables
